13f9bce2fbdca5f6e86886e0867bd747ee2ea1cf
fadeyush <a.fadeyush@gmail.com>
2022-07-08 12:00:51 +0300

    Подключен JS файл отдельно

---
+++ src/popup.js
@@ -0,0 +1,16 @@
+let summaryToggleOpen = document.querySelector(".button__more");
+let summaryToggleClose = document.querySelector(".summary__button");
+let summarySection = document.querySelector(".order__summary");
+
+summaryToggleOpen.addEventListener("click", function () {
+  if (summarySection.classList.contains("order__summary--closed")) {
+    summarySection.classList.remove("order__summary--closed");
+    summarySection.classList.add("order__summary--opened");
+  };
+});
+summaryToggleClose.addEventListener("click", function () {
+  if (summarySection.classList.contains("order__summary--opened")) {
+    summarySection.classList.remove("order__summary--opened");
+    summarySection.classList.add("order__summary--closed");
+  }
+});
