59ad357eea775f2de2c4d7956e8fcce325f0835e
fadeyush <a.fadeyush@gmail.com>
2022-07-08 12:06:05 +0300

    Исправлены ошибки

--- popup.js
+++ popup.js
@@ -1,16 +1,18 @@
-let summaryToggleOpen = document.querySelector(".button__more");
-let summaryToggleClose = document.querySelector(".summary__button");
-let summarySection = document.querySelector(".order__summary");
+(function () {
+  let summaryToggleOpen = document.querySelector(".button__more");
+  let summaryToggleClose = document.querySelector(".summary__button");
+  let summarySection = document.querySelector(".order__summary");
 
-summaryToggleOpen.addEventListener("click", function () {
-  if (summarySection.classList.contains("order__summary--closed")) {
-    summarySection.classList.remove("order__summary--closed");
-    summarySection.classList.add("order__summary--opened");
-  };
-});
-summaryToggleClose.addEventListener("click", function () {
-  if (summarySection.classList.contains("order__summary--opened")) {
-    summarySection.classList.remove("order__summary--opened");
-    summarySection.classList.add("order__summary--closed");
-  }
-});
+  summaryToggleOpen.addEventListener("click", function () {
+    if (summarySection.classList.contains("order__summary--closed")) {
+      summarySection.classList.remove("order__summary--closed");
+      summarySection.classList.add("order__summary--opened");
+    };
+  });
+  summaryToggleClose.addEventListener("click", function () {
+    if (summarySection.classList.contains("order__summary--opened")) {
+      summarySection.classList.remove("order__summary--opened");
+      summarySection.classList.add("order__summary--closed");
+    }
+  });
+}());
